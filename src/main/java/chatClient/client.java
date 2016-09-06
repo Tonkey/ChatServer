5cfe20f8798984d0048a27d030b92ffc56f0e7ff
Michael Larsen <Michael Larsen>
2016-09-06 14:23:58 +0200

    more client stuff

--- src/main/java/chatClient/client.java
+++ src/main/java/chatClient/client.java
@@ -1,27 +1,99 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package chatClient;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.Scanner;
 
 /**
  *
  * @author Michael
  */
 public class client {
+
+    static Socket socket;
+    static private int port;
+    static private InetAddress serverAddress;
+    static private String ip;
     
-    
-    public String timeStamp() {
-       Calendar calendar = new GregorianCalendar();
-       int HOUR = calendar.get(Calendar.HOUR);
-       int MINUTE = calendar.get(Calendar.MINUTE);
-       int SECOND = calendar.get(Calendar.SECOND);
-       
-       return HOUR + ":" + MINUTE + ":" + SECOND + ": ";
-       
+    static private Scanner input;
+    static private PrintWriter output;
+
+    public static void main(String[] args) throws IOException {
+
+        if (args.length == 2) {
+            ip = args[0];
+            port = Integer.parseInt(args[1]);
+        }
+
+        connect(ip, port);
+        startChat();
+    }
+
+    public static void connect(String address, int port) throws UnknownHostException, IOException {
+        port = 7777;
+        serverAddress = InetAddress.getByName(address);
+        socket = new Socket(serverAddress, port);
+        input = new Scanner(socket.getInputStream());
+        output = new PrintWriter(socket.getOutputStream(), true);  //Set to true, to get auto flush behaviour
+    }
+
+    private void Login() {
+
+    }
+
+    private void Logout() {
+
+    }
+
+    private String sendMSG(String msg) {
+
+        return msg;
+    }
+
+    private static void startChat() {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(chatGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(chatGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(chatGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(chatGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new chatGUI().setVisible(true);
+            }
+        });
+    }
+
+    private static String timeStamp() {
+        Calendar calendar = new GregorianCalendar();
+        int HOUR = calendar.get(Calendar.HOUR);
+        int MINUTE = calendar.get(Calendar.MINUTE);
+        int SECOND = calendar.get(Calendar.SECOND);
+
+        return HOUR + ":" + MINUTE + ":" + SECOND + ": <some username>";
+
     }
 }
